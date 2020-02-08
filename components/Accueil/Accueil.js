6f129adffa274b4415352695e43dc89c7d4ca7b6
YoannC <yoanngoo@gmail.com>
2020-02-09 00:41:11 +0100

    Initial commit

---
+++ components/Accueil/Accueil.js
@@ -0,0 +1,11 @@
+// Ici, vous pouvez ajouter du javascript si besoin. Exemple tout bête avec les boutons des réseaux sociaux :
+document
+  .getElementById("facebook-button")
+  .addEventListener("click", () =>
+    window.open("https://www.facebook.com/1024.media/", "_blank")
+  );
+document
+  .getElementById("instagram-button")
+  .addEventListener("click", () =>
+    window.open("https://www.instagram.com/instagram/", "_blank")
+  );
