893b8a1f375ca9f64c1ece60d22a1f5370a8b727
Yoann CROUIN <crouinicrouina@gmail.com>
2020-02-10 17:08:14 +0100

    choix ameloio

--- components/Puberte/8_Choix/Choix.js
+++ components/Puberte/8_Choix/Choix.js
@@ -1,5 +1,11 @@
 const { x, y } = document.getElementById("panier").getBoundingClientRect();
 
+document.querySelector("body").addEventListener("click", () => {
+  document
+    .querySelectorAll(".un-choix")
+    .forEach(el => el.classList.remove("showText"));
+});
+
 document.querySelectorAll(".un-choix").forEach(el => {
   // hide text
   el.addEventListener("dragstart", () => {
@@ -7,9 +13,10 @@ document.querySelectorAll(".un-choix").forEach(el => {
       .querySelectorAll(".un-choix")
       .forEach(el => el.classList.remove("showText"));
   });
+
   // show text
   el.addEventListener("dragend", e => {
-    console.log(e.x);
+    console.log(e.x, e.y, x, y);
     if (el.id !== "couche" && e.x > x && e.y > y) {
       el.classList.toggle("showText");
     }
